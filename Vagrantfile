076613cf89270d5fcf6b8096acbad5e81ab1fdfc
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-01-20 20:17:56 +0300

    ref(vagrant): fix box version & install etcd only once

--- Vagrantfile
+++ Vagrantfile
@@ -3,4 +3,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "archlinux/archlinux"
   config.vm.provision :shell, path: "chore/bootstrap.sh", keep_color: false
+  config.vm.box_version = "2018.01.07"
 end
--- chore/bootstrap.sh
+++ chore/bootstrap.sh
@@ -7,31 +7,26 @@ DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
 ETCD_ARCHIVE=/tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 ETCD_PATH=/opt/etcd
 
-# instal and check etcd
-[ -f "$ETCD_ARCHIVE" ] && rm ${ETCD_ARCHIVE}
-echo downloading etcd archive
-curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz \
-    -o ${ETCD_ARCHIVE} &> /dev/null
-[ -d "$ETCD_PATH" ] && rm -rf ${ETCD_PATH}
-mkdir ${ETCD_PATH}
-echo installing etcd
-tar xzvf ${ETCD_ARCHIVE} \
-    -C ${ETCD_PATH} --strip-components=1 &> /dev/null
-echo cleaning up
-rm -f ${ETCD_ARCHIVE}
-echo setting up path
-ln -sf "$ETCD_PATH/etcd" /bin/etcd
-ln -sf "$ETCD_PATH/etcdctl" /bin/etcdctl
-echo checking instalation
+if [ -x "$ETCD_PATH/etcd" -a -x "$ETCD_PATH/etcdctl" ]; then
+    echo etcd is installed
+else
+    [ -f "$ETCD_ARCHIVE" ] && rm ${ETCD_ARCHIVE}
+    echo downloading etcd archive
+    curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz \
+        -o ${ETCD_ARCHIVE} &> /dev/null
+    [ -d "$ETCD_PATH" ] && rm -rf ${ETCD_PATH}
+    mkdir ${ETCD_PATH}
+    echo installing etcd
+    tar xzvf ${ETCD_ARCHIVE} \
+        -C ${ETCD_PATH} --strip-components=1 &> /dev/null
+    echo cleaning up
+    rm -f ${ETCD_ARCHIVE}
+    echo setting up path
+    ln -sf "$ETCD_PATH/etcd" /bin/etcd
+    ln -sf "$ETCD_PATH/etcdctl" /bin/etcdctl
+    echo checking instalation
+fi
+
 etcd --version
 etcdctl --version
-
-echo set up mirrorlist
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-
-echo full system update
-yes | pacman -Syyu &> /dev/null
-
 echo done
