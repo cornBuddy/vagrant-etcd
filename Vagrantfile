a02502332a907c5c05d94bdb63b8bb8c8d5fdb44
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-01-25 10:52:36 +0300

    chore(bootstrap): roll down etcd cluster, move to centos

--- Vagrantfile
+++ Vagrantfile
@@ -1,16 +1,41 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-VM_BOX = "archlinux/archlinux"
-BOX_VERSION = "2018.01.07"
-ETCD_CLUSTERS = ["infra0", "infra1", "infra2"]
+VM_BOX = "centos/7"
+BOX_VERSION = "1801.02"
+ETCD_SERVER_PORT = "2380"
+ETCD_CLIENT_PORT = "2379"
+ETCD_INITIAL_CLUSTER_TOKEN = "etcd-cluster"
+ETCD_INITIAL_CLUSTER_STATE = "new"
+ETCD_CLUSTERS = [
+  { name: "infra1", ip_addr: "192.168.0.11" },
+  { name: "infra2", ip_addr: "192.168.0.12" },
+  { name: "infra3", ip_addr: "192.168.0.13" },
+]
+ETCD_INITIAL_CLUSTER = ETCD_CLUSTERS
+  .map do |cluster_config|
+    "#{cluster_config[:name]}=http://#{cluster_config[:ip_addr]}"\
+      ":#{ETCD_SERVER_PORT}"
+  end
+  .join(",")
 
 Vagrant.configure("2") do |config|
-  ETCD_CLUSTERS.each do |cluster_name|
-    config.vm.define cluster_name do |cluster|
+  ETCD_CLUSTERS.each do |cluster_config|
+    config.vm.define cluster_config[:name] do |cluster|
+      cluster.vm.network "private_network", ip: cluster_config[:ip_addr]
       cluster.vm.box = VM_BOX
-      cluster.vm.provision :shell, path: "chore/bootstrap.sh", keep_color: false
       cluster.vm.box_version = BOX_VERSION
+      env = {
+        "ETCD_IP" => cluster_config[:ip_addr],
+        "ETCD_NAME" => cluster_config[:name],
+        "ETCD_SERVER_PORT" => ETCD_SERVER_PORT,
+        "ETCD_CLIENT_PORT" => ETCD_CLIENT_PORT,
+        "ETCD_INITIAL_CLUSTER_TOKEN" => ETCD_INITIAL_CLUSTER_TOKEN,
+        "ETCD_INITIAL_CLUSTER" => ETCD_INITIAL_CLUSTER,
+        "ETCD_INITIAL_CLUSTER_STATE" => ETCD_INITIAL_CLUSTER_STATE,
+      }
+      cluster.vm.provision :shell, path: "chore/bootstrap.sh",
+        keep_color: false, env: env
     end
   end
 end
--- chore/bootstrap.sh
+++ chore/bootstrap.sh
@@ -3,24 +3,47 @@
 set -e
 
 ETCD_VER=v3.2.14
-DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
+GITHUB_ETCD=https://github.com/coreos/etcd/releases/download
+DOWNLOAD_URL="$GITHUB_ETCD/$ETCD_VER/etcd-$ETCD_VER-linux-amd64.tar.gz"
 ETCD_ARCHIVE=/tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 ETCD_PATH=/opt/etcd
+ETCD_CONFIG_PATH=/etc/etcd/etcd.conf
+ETCD_HOME=/var/lib/etcd/
+ETCD_DATA_DIR="$ETCD_HOME/default.etcd"
+ETCD_SERVICE_PATH=/etc/systemd/system/etcd.service
+
+ETCD_SERVER="http://$ETCD_IP:$ETCD_SERVER_PORT"
+ETCD_CLIENT="http://$ETCD_IP:$ETCD_CLIENT_PORT"
+read -r -d '' ETCD_ENV << EOF || true
+# [member]
+ETCD_NAME=$ETCD_NAME
+ETCD_DATA_DIR=$ETCD_DATA_DIR
+ETCD_LISTEN_CLIENT_URLS=$ETCD_CLIENT,http://127.0.0.1:$ETCD_CLIENT_PORT
+ETCD_ADVERTISE_CLIENT_URLS=$ETCD_CLIENT
+ETCD_LISTEN_PEER_URLS=$ETCD_SERVER
+# [cluster]
+ETCD_INITIAL_ADVERTISE_PEER_URLS=$ETCD_SERVER
+ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER
+ETCD_INITIAL_CLUSTER_TOKEN=$ETCD_INITIAL_CLUSTER_TOKEN
+ETCD_INITIAL_CLUSTER_STATE=$ETCD_INITIAL_CLUSTER_STATE
+EOF
 
 read -r -d '' ETCD_SERVICE << EOF || true
 [Unit]
-Description=Etcd service
+Description=etcd
+Documentation=https://github.com/coreos/etcd
 After=network.target
+
 [Service]
-Type=simple
-PIDFile=/var/run/etcd.pid
-WorkingDirectory=/opt/etcd
-User=root
-Group=root
-ExecStart=/usr/bin/etcd
-ExecStop=/usr/bin/kill -15 `/usr/bin/pidof etcd`
-ExecReload=/usr/bin/kill -15 `/usr/bin/pidof etcd` ; /usd/bin/etcd
+Type=notify
+WorkingDirectory=$ETCD_HOME
+EnvironmentFile=-$ETCD_CONFIG_PATH
+User=etcd
+ExecStart=$ETCD_PATH/etcd
+LimitNOFILE=65536
 Restart=always
+RestartSec=5
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -33,43 +56,61 @@ is_etcd_installed() {
     fi
 }
 
-install_and_setup_etcd() {
-    [ -f "$ETCD_ARCHIVE" ] && rm ${ETCD_ARCHIVE}
-    echo downloading etcd archive
-    curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz \
-        -o ${ETCD_ARCHIVE} &> /dev/null
-    [ -d "$ETCD_PATH" ] && rm -rf ${ETCD_PATH}
-    mkdir ${ETCD_PATH}
-    echo installing etcd
-    tar xzvf ${ETCD_ARCHIVE} \
-        -C ${ETCD_PATH} --strip-components=1 &> /dev/null
-    echo cleaning up
-    rm -f ${ETCD_ARCHIVE}
-    echo setting up path
-    ln -sf "$ETCD_PATH/etcd" /bin/etcd
-    ln -sf "$ETCD_PATH/etcdctl" /bin/etcdctl
-    echo "export ETCDCTL_API=3" >> /etc/profile
+is_etcd_service_created() {
+    if [ -f $ETCD_SERVICE_PATH ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
-create_etcd_service() {
+configure_etcd_service() {
     echo creating etcd service
-    echo "$ETCD_SERVICE" > /etc/systemd/system/etcd.service
+    echo "$ETCD_ENV" > $ETCD_CONFIG_PATH
+    echo "$ETCD_SERVICE" > $ETCD_SERVICE_PATH
+    systemctl daemon-reload
 }
 
 enable_etcd_service() {
     echo enabling and starting etcd service
-    systemctl enable etcd.service
-    systemctl start etcd.service
+    systemctl enable --no-block etcd.service
+    systemctl start --no-block etcd.service
+}
+
+install_and_configure_etcd() {
+    echo downloading etcd archive
+    curl -L $DOWNLOAD_URL -o $ETCD_ARCHIVE &> /dev/null
+    mkdir $ETCD_PATH
+    echo installing etcd
+    tar xzvf $ETCD_ARCHIVE -C $ETCD_PATH --strip-components=1 &> /dev/null
+    echo cleaning up
+    rm -f $ETCD_ARCHIVE
+    echo setting up path
+    ln -sf "$ETCD_PATH/etcd" /bin/etcd
+    ln -sf "$ETCD_PATH/etcdctl" /bin/etcdctl
+    echo "export ETCDCTL_API=3" >> /etc/profile
+    echo basic configuration
+    mkdir $ETCD_HOME
+    mkdir /etc/etcd
+    groupadd --system etcd
+    useradd --system \
+        --gid etcd \
+        --home-dir $ETCD_HOME \
+        --shell /sbin/nologin \
+        --comment "etcd user" \
+        etcd
+    chown -R etcd:etcd $ETCD_HOME
 }
 
 if is_etcd_installed; then
     echo etcd is installed
 else
-    install_and_setup_etcd
-    create_etcd_service
-    enable_etcd_service
-    echo checking instalation
-    etcd --version
+    install_and_configure_etcd
 fi
 
-echo done
+if is_etcd_service_created; then
+    echo etcd service created
+else
+    configure_etcd_service
+    enable_etcd_service
+fi
